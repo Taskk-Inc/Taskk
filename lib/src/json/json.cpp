24a1ce3dad44779c5ea7ab688ea481c49009cac9
Nepsis <berriot.cesar@gmail.com>
2024-03-25 21:30:23 +0100

    added & linked jansson

---
+++ lib/src/json/json.cpp
@@ -0,0 +1,10 @@
+//
+// Created by César B. on 3/25/2024.
+//
+
+#include "json.hpp"
+
+std::string json::encode_session(data_types::session)
+{
+	return std::string();
+}
---
+++ lib/src/json/json.hpp
@@ -0,0 +1,12 @@
+//
+// Created by César B. on 3/25/2024.
+//
+
+#pragma once
+
+#include <string>
+#include "data_types/data_types.hpp"
+
+namespace json
+{	std::string encode_session(data_types::session);
+}

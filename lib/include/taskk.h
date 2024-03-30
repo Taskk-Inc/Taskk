3c2674b697efaf2eb348485693dd17e22e6e6469
Nepsis <berriot.cesar@gmail.com>
2024-03-30 03:47:09 +0100

    implemented custom operation data into the c api

--- lib/include/taskk.h
+++ lib/include/taskk.h
@@ -6,5 +6,11 @@
 
 #include "taskk/c/taskk_api.h"
 
+enum class taskk_data_type { taskk_string, taskk_integer, taskk_real, taskk_boolean, };
+
 TASKK_API void taskk_begin_operation(char * name);
+TASKK_API void taskk_attach_string(char * data);
+TASKK_API void taskk_attach_integer(int data);
+TASKK_API void taskk_attach_real(double data);
+TASKK_API void taskk_attach_boolean(bool data);
 TASKK_API void taskk_end_operation(char * name);
--- lib/src/taskk/c_api.cpp
+++ lib/src/taskk/c_api.cpp
@@ -9,6 +9,22 @@ __declspec(dllexport) void taskk_begin_operation(char * name)
 {	logic::begin_operation(name);
 }
 
+__declspec(dllexport) void taskk_attach_string(char * data)
+{	logic::attach_operation_string(data);
+}
+
+__declspec(dllexport) void taskk_attach_integer(int data)
+{	logic::attach_operation_integer(data);
+}
+
+__declspec(dllexport) void taskk_attach_real(double data)
+{	logic::attach_operation_real(data);
+}
+
+__declspec(dllexport) void taskk_attach_boolean(bool data)
+{	logic::attach_operation_boolean(data);
+}
+
 __declspec(dllexport) void taskk_end_operation(char * name)
 {	logic::end_operation(name);
 }
